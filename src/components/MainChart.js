1d3ee3b5c2ab04faec0bb97a45e9ac00840762fa
ben <oluwatonilobailesanmi0001@gmail.com>
2026-08-28 08:43:35 +0100

    refactor: update TypeScript ignore annotations and imports in MainChart component

--- src/components/MainChart.js
+++ src/components/MainChart.js
@@ -1,8 +1,7 @@
 import { jsx as _jsx } from "react/jsx-runtime";
+// @ts-nocheck
 import { useEffect, useMemo, useState } from 'react';
-// @ts-ignore
 import { Chart } from 'react-chartjs-2';
-// @ts-ignore
 import { BarController, BarElement, CategoryScale, Chart as ChartJS, Filler, Legend, LinearScale, LineController, LineElement, PointElement, Tooltip, } from 'chart.js';
 ChartJS.register(LineController, LineElement, PointElement, BarController, BarElement, CategoryScale, LinearScale, Legend, Tooltip, Filler);
 const API = import.meta.env.VITE_API_BASE_URL;
